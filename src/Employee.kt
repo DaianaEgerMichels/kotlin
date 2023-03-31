4fa7229d36fc2fb0c24c3bedc9421de09668de56
Daiana Michels <daianaem.12@gmail.com>
2023-03-30 21:42:55 -0300

    adds type employee

--- src/Employee.kt
+++ src/Employee.kt
@@ -1,9 +1,22 @@
 class Employee(
     val name: String,
     val cpf: String,
-    val salary: Double
+    val salary: Double,
+    val type: Int //0 employee, 1 manager, 2 director
 ){
     fun bonus(): Double {
-        return salary * 0.1
+        return when (type) {
+            0 -> {
+                salary * 0.1
+            }
+
+            2 -> {
+                salary * 0.2
+            }
+
+            else -> {
+                salary * 0.3
+            }
+        }
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -5,11 +5,13 @@ fun main() {
     val employee1 = Employee(
         name = "Cristal Lockshelf",
         cpf = "111.222.333-44",
-        salary = 2000.0
+        salary = 2000.0,
+        type = 0
     )
 
     println("name ${employee1.name}")
     println("cpf ${employee1.cpf}")
     println("salary ${employee1.salary}")
     println("bonus ${employee1.bonus()}")
+    println("type ${employee1.type}")
 }
